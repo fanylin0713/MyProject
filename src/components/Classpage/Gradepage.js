791dd2516ea9b3dbb3f13e692d81072eda8f73c2
Fanylin <fanylin0713@gmail.com>
2019-03-21 17:34:56 +0800

    成績

--- src/components/Classpage/Gradepage.js
+++ src/components/Classpage/Gradepage.js
@@ -1,11 +1,43 @@
 import React, { Component } from 'react';
 
+const data = [{
+    name: 'name'
+}]
+
 class Gradepage extends Component {
     render() {
+        if (data.length) {
+            return (
+            <div>
+                <div>title</div>
+                <ul>
+                    {data.map((el, index) => (
+                        <li key={index}>{el.name}</li>
+                    ))}
+                </ul>    
+            </div>
+            )
+        }
         return (
-            <div>這是成績輸入那頁
+            <div>
+                <div>title</div>
+                新增資料 
             </div>
         );
+        // return (
+        //     <div>
+        //         <div>title</div>
+        //         {
+        //             data.length ? 
+        //             <ul>
+        //                 {data.map((el, index) => (
+        //                     <li key={index}>{el.name}</li>
+        //                 ))}
+        //             </ul> :
+        //             '新增資料'
+        //         }    
+        //     </div>
+        // );
     }
 }
 
