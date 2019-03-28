2053bedec565dace0390b55a2ebbe403514de022
Fanylin <fanylin0713@gmail.com>
2019-03-28 17:07:36 +0800

    校區選擇

--- src/components/Appbar/Appbar.js
+++ src/components/Appbar/Appbar.js
@@ -3,10 +3,19 @@ import PropTypes from 'prop-types';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
 import InputBase from '@material-ui/core/InputBase';
 import { fade } from '@material-ui/core/styles/colorManipulator';
 import { withStyles } from '@material-ui/core/styles';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Input from '@material-ui/core/Input';
+
 
 import pin from './pin.png';
 import Drawer from '../Drawer/Drawer';
@@ -28,14 +37,6 @@ const styles = theme => ({
     menuButton: {
         marginLeft: -12,
         marginRight: 20,
-    },
-    title: {
-        display: 'none',
-        fontSize: '18px',
-        margin: '0 0 0 10px',
-        [theme.breakpoints.up('sm')]: {
-            display: 'block',
-        },
     },
     search: {
         position: 'relative',
@@ -83,47 +84,113 @@ const styles = theme => ({
             },
         },
     },
+
+    formControl: {
+        margin: theme.spacing.unit,
+        minWidth: 120,
+    },
 });
 
-function SearchAppBar(props) {
-    const { classes } = props;
-    return (
-        <div className={classes.root}>
-            <AppBar position="static" style={{ backgroundColor: '#111B24' }}>
-                <Toolbar>
-                    {/* <SideBarIcon className={classes.menuButton} onClick={this.toggleDrawer('left', true)} /> */}
-                    <Drawer />
-                    <img src={pin} height="26" alt="location"></img>
-                    {/* <Selector /> */}
-                    <Typography className={classes.title} color="inherit" noWrap>
-                        台北校區
-            </Typography>
-                    <NavLink activeClassName="active" to="/">
-                        <Button className={classes.button}><Home /></Button></NavLink>
-                    <Button className={classes.button}><Face /></Button>
-                    <NavLink style={{ textDecoration: 'none' }} activeClassName="active" to="/login">
-                        <Button className={classes.button}>
-                            <LogoutIcon className={classes.rightIcon} />
-                            Log out
-            </Button></NavLink>
-                    <div className={classes.grow} />
-                    <div className={classes.search}>
-                        <div className={classes.searchIcon}>
-                            <SearchIcon />
+
+class SearchAppBar extends React.Component {
+
+    state = {
+        open: false,
+        area: '',
+    };
+
+    handleChange = name => event => {
+        this.setState({ [name]: Number(event.target.value) });
+    };
+
+    handleClickOpen = () => {
+        this.setState({ open: true });
+    };
+
+    handleClose = () => {
+        this.setState({ open: false });
+    };
+
+
+    render() {
+
+        const { classes } = this.props;
+        return (
+            <div className={classes.root}>
+                <AppBar position="static" style={{ backgroundColor: '#111B24' }}>
+                    <Toolbar>
+                        {/* <SideBarIcon className={classes.menuButton} onClick={this.toggleDrawer('left', true)} /> */}
+                        <Drawer />
+                        <img src={pin} height="26" alt="location"></img>
+                        {/* <Selector /> */}
+                        {/* 校區選擇 */}
+                        <div>
+                            <Button onClick={this.handleClickOpen}>北投校區</Button>
+                            <Dialog
+                                disableBackdropClick
+                                disableEscapeKeyDown
+                                open={this.state.open}
+                                onClose={this.handleClose}
+                            >
+                                <DialogTitle>選擇校區</DialogTitle>
+                                <DialogContent>
+                                    <form className={classes.container}>
+                                        <FormControl className={classes.formControl}>
+                                            <Select
+                                                value={this.state.age}
+                                                onChange={this.handleChange('area')}
+                                                input={<Input id="age-simple" />}
+                                            >
+                                                <MenuItem value="">
+                                                    <em>校區</em>
+                                                </MenuItem>
+                                                <MenuItem value={10}>北投校區</MenuItem>
+                                                <MenuItem value={20}>士林校區</MenuItem>
+                                                <MenuItem value={30}>南港校區</MenuItem>
+                                            </Select>
+                                        </FormControl>
+                                    </form>
+                                </DialogContent>
+                                <DialogActions>
+                                    <Button onClick={this.handleClose} color="primary">
+                                        Cancel
+                                    </Button>
+                                    <Button onClick={this.handleClose} color="primary">
+                                        Ok
+                                    </Button>
+                                </DialogActions>
+                            </Dialog>
                         </div>
-                        <InputBase
-                            placeholder="學號查詢…"
-                            classes={{
-                                root: classes.inputRoot,
-                                input: classes.inputInput,
-                            }}
-                        />
-                    </div>
-                </Toolbar>
-            </AppBar>
-        </div>
-    );
+                        {/* 校區選擇 */}
+                        <NavLink activeClassName="active" to="/">
+                            <Button className={classes.button}><Home /></Button></NavLink>
+                        <Button className={classes.button}><Face /></Button>
+                        <NavLink style={{ textDecoration: 'none' }} activeClassName="active" to="/login">
+                            <Button className={classes.button}>
+                                <LogoutIcon className={classes.rightIcon} />
+                                Log out
+                            </Button>
+                        </NavLink>
+                        <div className={classes.grow} />
+                        <div className={classes.search}>
+                            <div className={classes.searchIcon}>
+                                <SearchIcon />
+                            </div>
+                            <InputBase
+                                placeholder="學號查詢…"
+                                classes={{
+                                    root: classes.inputRoot,
+                                    input: classes.inputInput,
+                                }}
+                            />
+                        </div>
+                    </Toolbar>
+                </AppBar>
+            </div>
+        );
+    }
 }
+
 
 SearchAppBar.propTypes = {
     classes: PropTypes.object.isRequired,
