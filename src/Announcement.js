344c618f2dad1c74cbfbc2f3ef820739a64552ad
Fanylin <fanylin0713@gmail.com>
2019-04-01 17:25:28 +0800

    Create Announcement.js

---
+++ src/Announcement.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import AppBar from './components/AppBar/Appbar'
+
+
+class Announcement extends React.Component {
+    
+    render() {
+        
+        return (
+            <div>
+            <AppBar />
+            </div>
+        )
+    }
+
+}
+
+export default Announcement;
