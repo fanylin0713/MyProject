0ae87beb0e7e583b4b57be9a20b99900712270e2
Fanylin <fanylin0713@gmail.com>
2019-04-08 13:14:00 +0800

    Create MakeupClass.js

---
+++ src/MakeupClass.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import AppBar from './components/AppBar/Appbar'
+
+
+class MakeupClass extends React.Component {
+    
+    render() {
+        
+        return (
+            <div>
+            <AppBar />
+            </div>
+        )
+    }
+
+}
+
+export default MakeupClass;
