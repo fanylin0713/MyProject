81b235a20578273f364db0b0c2c2a5ba7f120d1d
Fanylin <fanylin0713@gmail.com>
2019-04-08 14:10:03 +0800

    announce

--- src/Announcement.js
+++ src/Announcement.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import AppBar from './components/AppBar/Appbar'
-import AnnounceTable from './components/Announce/AnnounceTable';
+import AnnounceTable from './components/Table/AnnounceTable';
 
 import AnnounceDialog from './components/Dialog/AnnounceDialog';
 
