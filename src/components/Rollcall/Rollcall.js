3c44337a297b730fd923bffbb6cd45f563ad7d50
Fanylin <fanylin0713@gmail.com>
2019-04-30 15:23:07 +0800

    Update Rollcall.js

--- src/components/Rollcall/Rollcall.js
+++ src/components/Rollcall/Rollcall.js
@@ -18,6 +18,9 @@ const styles = theme => ({
   label: {
     fontSize: '14pt',
   },
+  nowClass:{
+    width:'140px'
+  },
 });
 
 class Rollcall extends React.Component {
@@ -25,11 +28,10 @@ class Rollcall extends React.Component {
   state = {
     value: '',
     nowClass: '',
-    name: 's'
   };
 
-  handleChange = event => {
-    this.setState({ value: event.target.value });
+  handleChange = name => event => {
+    this.setState({ [name] : event.target.value });
   };
 
   render() {
@@ -44,11 +46,40 @@ class Rollcall extends React.Component {
               name="area"
               className={classes.group}
               value={this.state.value}
-              onChange={this.handleChange}
+              onChange={this.handleChange('value')}
             >
               <FormControlLabel classes={{ label: classes.label, }} value="國中" control={<Radio />} label="國中" />
               <FormControlLabel classes={{ label: classes.label, }} value="高中" control={<Radio />} label="高中" />
             </RadioGroup>
+          </FormControl>
+          <FormControl variant="outlined" className={classes.formControl}>
+            <InputLabel
+              ref={ref => {
+                this.InputLabelRef = ref;
+              }}
+              htmlFor="outlined-nowClass-simple"
+            >
+            年級
+                    </InputLabel>
+            <Select
+              value={this.state.nowClass}
+              onChange={this.handleChange('nowClass')}
+              className={classes.nowClass}
+              input={
+                <OutlinedInput
+                  labelWidth={this.state.labelWidth}
+                  name="nowClass"
+                  id="outlined-nowClass-simple"
+                />
+              }
+            >
+              <MenuItem value="">
+                <em>年級</em>
+              </MenuItem>
+              <MenuItem value={10}>英文</MenuItem>
+              <MenuItem value={20}>數學</MenuItem>
+              <MenuItem value={30}>國文</MenuItem>
+            </Select>
           </FormControl>
         </div>
       </div>
