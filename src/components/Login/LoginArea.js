9fd98f15d8a8a22c13007c94e91d0dd9d4ab88ce
Fanylin <fanylin0713@gmail.com>
2019-05-01 10:53:36 +0800

    login 排版

--- src/components/Login/LoginArea.js
+++ src/components/Login/LoginArea.js
@@ -9,14 +9,16 @@ import FormLabel from '@material-ui/core/FormLabel';
 
 const styles = theme => ({
     root:{
-        display:'flex',
-        flexDirection:'row',
+        // width:'80%',
     },
     title: {
-        fontSize: '16pt',
+        fontSize: '18pt',
     },
     label: {
-        fontSize: '14pt',
+        fontSize: '16pt',
+    },
+    area:{
+        marginLeft: theme.spacing.unit * 3,
     },
 });
 
@@ -41,16 +43,17 @@ class AreaPick extends React.Component {
                         className={classes.group}
                         value={this.state.value}
                         onChange={this.handleChange}
+                        row
                     >
-                        <FormControlLabel classes={{label: classes.label,}} value="台北校區" control={<Radio />} label="台北校區" />
-                        <FormControlLabel classes={{label: classes.label,}} value="古亭校區" control={<Radio />} label="古亭校區" />
-                        <FormControlLabel classes={{label: classes.label,}} value="信義校區" control={<Radio />} label="信義校區" />
-                        <FormControlLabel classes={{label: classes.label,}} value="高雄校區" control={<Radio />} label="高雄校區" />
-                        <FormControlLabel classes={{label: classes.label,}} value="南港校區" control={<Radio />} label="南港校區" />
-                        <FormControlLabel classes={{label: classes.label,}} value="中山校區" control={<Radio />} label="中山校區" />
-                        <FormControlLabel classes={{label: classes.label,}} value="士林校區" control={<Radio />} label="士林校區" />
-                        <FormControlLabel classes={{label: classes.label,}} value="新莊校區" control={<Radio />} label="新莊校區" />
-                        <FormControlLabel classes={{label: classes.label,}} value="板橋校區" control={<Radio />} label="板橋校區" />
+                        <FormControlLabel className={classes.area} classes={{label: classes.label,}} value="台北校區" control={<Radio />} label="台北校區" />
+                        <FormControlLabel className={classes.area} classes={{label: classes.label,}} value="古亭校區" control={<Radio />} label="古亭校區" />
+                        <FormControlLabel className={classes.area} classes={{label: classes.label,}} value="信義校區" control={<Radio />} label="信義校區" />
+                        <FormControlLabel className={classes.area} classes={{label: classes.label,}} value="高雄校區" control={<Radio />} label="高雄校區" />
+                        <FormControlLabel className={classes.area} classes={{label: classes.label,}} value="南港校區" control={<Radio />} label="南港校區" />
+                        <FormControlLabel className={classes.area} classes={{label: classes.label,}} value="中山校區" control={<Radio />} label="中山校區" />
+                        <FormControlLabel className={classes.area} classes={{label: classes.label,}} value="士林校區" control={<Radio />} label="士林校區" />
+                        <FormControlLabel className={classes.area} classes={{label: classes.label,}} value="新莊校區" control={<Radio />} label="新莊校區" />
+                        <FormControlLabel className={classes.area} classes={{label: classes.label,}} value="板橋校區" control={<Radio />} label="板橋校區" />
                     </RadioGroup>
                 </FormControl>
             </div>
