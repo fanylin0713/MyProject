e0626eb9f301952e544a7153221c84842876582f
fabienne <39395133+Fabeinne@users.noreply.github.com>
2019-05-07 00:18:01 +0800

    student airtable

--- src/components/Classpage/Studentpage.js
+++ src/components/Classpage/Studentpage.js
@@ -12,6 +12,9 @@ import { NavLink } from "react-router-dom";
 // import { fromJS, List, Map } from 'immutable'
 import XLSX from 'xlsx';
 import ArrowIcon from '@material-ui/icons/ArrowForwardRounded';
+import Airtable from 'airtable';
+
+const base = new Airtable({ apiKey: 'keyA7EKdngjou4Dgy' }).base('appcXtOTPnE4QWIIt');
 
 const styles = theme => ({
     root: {
@@ -46,19 +49,33 @@ const styles = theme => ({
     },
 });
 
+// let id = 0;
+// function createData(time, title, content) {
+//     id += 1;
+//     return { id, time, title, content };
+// }
+
 let id = 0;
-function createData(time, title, content) {
+function createData(stuId, stuName) {
     id += 1;
-    return { id, time, title, content };
+    //this.props.callbackFromParent();
+    const content = <NavLink style={{ textDecoration: 'none' }} activeClassName="active" to={{pathname:'/student', aboutProps:{name:stuId}}}><Button>更多資訊<ArrowIcon/></Button></NavLink>
+    //const content = <NavLink style={{ textDecoration: 'none' }} activeClassName="active" to="/student"><Button>更多資訊<ArrowIcon/></Button></NavLink>
+    return { id, stuId, stuName, content };
 }
 
-const data = [
-    createData('405401360', '林ＸＸ', <NavLink style={{ textDecoration: 'none' }} activeClassName="active" to="/student"><Button>更多資訊<ArrowIcon/></Button></NavLink>),
-    createData('405401360', '林ＸＸ', <NavLink style={{ textDecoration: 'none' }} activeClassName="active" to="/student"><Button>更多資訊<ArrowIcon/></Button></NavLink>),
-    createData('405401360', '林ＸＸ', <NavLink style={{ textDecoration: 'none' }} activeClassName="active" to="/student"><Button>更多資訊<ArrowIcon/></Button></NavLink>),
-]
+// const data = [
+//     // createData('405401360', '林ＸＸ', <NavLink style={{ textDecoration: 'none' }} activeClassName="active" to="/student"><Button>更多資訊<ArrowIcon/></Button></NavLink>),
+//     // createData('405401360', '林ＸＸ', <NavLink style={{ textDecoration: 'none' }} activeClassName="active" to="/student"><Button>更多資訊<ArrowIcon/></Button></NavLink>),
+//     // createData('405401360', '林ＸＸ', <NavLink style={{ textDecoration: 'none' }} activeClassName="active" to="/student"><Button>更多資訊<ArrowIcon/></Button></NavLink>),
+//     // createData('405401360', '林ＸＸ'),
+//     // createData('405401360', '林ＸＸ'),
+// ]
 
 class Studentpage extends Component {
+    state = {
+        data:[],
+    }
     onImportExcel = file => {
         const { files } = file.target;
         const fileReader = new FileReader();
@@ -81,6 +98,29 @@ class Studentpage extends Component {
         };
         fileReader.readAsBinaryString(files[0]);
     }
+
+    //airtable
+    componentDidMount() {
+        base('Student').select({view: 'Grid view'})
+        .eachPage(
+            (records, fetchNextPage) => {
+            this.setState({records});
+            console.log(records);
+            const student_id = this.state.records.map((record, index) => record.fields['student_id']);
+            const student_name = this.state.records.map((record, index) => record.fields['student_name']);
+
+            var temp = [];
+            for(var index = 0; index < student_id.length; index++) {
+                temp.push(createData(student_id[index],student_name[index]));  
+            }
+
+            this.setState({ data : temp });
+
+            fetchNextPage();
+            }
+        );
+
+    }
     render() {
         const { classes } = this.props;
         return (
@@ -93,7 +133,7 @@ class Studentpage extends Component {
                     </Button>
                 </div>
                 {
-                    data.length
+                    this.state.data.length
                         ?
                         <Paper className={classes.root}>
                             <Table className={classes.table}>
@@ -105,10 +145,10 @@ class Studentpage extends Component {
                                     </TableRow>
                                 </TableHead>
                                 <TableBody>
-                                    {data.map(row => (
+                                    {this.state.data.map(row => (
                                         <TableRow key={row.id}>
-                                            <TableCell className={classes.content} style={{ width: '15%' }} component="th" scope="row">{row.time}</TableCell>
-                                            <TableCell className={classes.content} style={{ width: '20%' }}>{row.title}</TableCell>
+                                            <TableCell className={classes.content} style={{ width: '15%' }} component="th" scope="row">{row.stuId}</TableCell>
+                                            <TableCell className={classes.content} style={{ width: '20%' }}>{row.stuName}</TableCell>
                                             <TableCell className={classes.content}>{row.content}</TableCell>
                                         </TableRow>
                                     ))}
--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -5,6 +5,11 @@ import { Card, Typography, Button } from '@material-ui/core';
 import CameraIcon from '@material-ui/icons/CameraAltRounded';
 import WarningIcon from '@material-ui/icons/Warning';
 import SnackbarContent from '@material-ui/core/SnackbarContent';
+import Airtable from 'airtable';
+
+const TABLE_NAME = 'Student';
+const base = new Airtable({ apiKey: 'keyA7EKdngjou4Dgy' }).base('appcXtOTPnE4QWIIt');
+const table = base(TABLE_NAME);
 
 const styles = theme => ({
     card: {
@@ -66,17 +71,65 @@ const styles = theme => ({
 });
 
 class Student extends React.Component {
+    state = {
+        stu_id: '',
+        stu_name: '',
+        stu_img: '',
+        stu_school: '',
+        stu_grade: '',
+        stu_address: '',
+        stu_birth: '',
+        stu_phone: '',
+        stu_email: '',
+        stu_parent: '',
+        stu_parent_phone: '',
+      };
+
+    //airtable
+    componentDidMount() {
+        
+        const fileterSentence = 'AND(student_id = ' + this.props.location.aboutProps.name + ')'
+        console.log(fileterSentence);
+        table.select({
+          filterByFormula: fileterSentence,
+          view: "Grid view",
+          maxRecords: 1
+          }).eachPage((records, fetchNextPage) => {
+            this.setState({records});
+    
+            const student_name = this.state.records.map((record, index) => record.fields['student_name']);
+            const student_id = this.state.records.map((record, index) => record.fields['student_id']);
+            const student_school = this.state.records.map((record, index) => record.fields['student_school']);
+            const student_grade = this.state.records.map((record, index) => record.fields['student_grade']);
+            const student_birth = this.state.records.map((record, index) => record.fields['student_birth']);
+            const student_phone = this.state.records.map((record, index) => record.fields['student_phone']);
+            const student_email = this.state.records.map((record, index) => record.fields['student_email']);
+            const student_address = this.state.records.map((record, index) => record.fields['student_address']);
+            const student_parent = this.state.records.map((record, index) => record.fields['student_parent']);
+            const student_parent_phone = this.state.records.map((record, index) => record.fields['student_parent_phone']);
+            const student_img = this.state.records.map((record, index) => record.fields['student_img'][0].url); 
+    
+            this.setState({ stu_id : student_id, stu_name : student_name, stu_img : student_img 
+            ,stu_school : student_school , stu_grade : student_grade, stu_birth: student_birth, stu_address: student_address,
+            stu_email : student_email, stu_phone : student_phone, stu_parent : student_parent, stu_parent_phone : student_parent_phone});
+            //this.setState({ stu_id : student_id });
+            fetchNextPage(); 
+          }
+          );
+      }
+    
     render() {
         const { classes } = this.props;
+        //console.log(this.props.location.aboutProps);
         return (
             <div>
                 <AppBar />
                 <div>
                     <Card className={classes.card} >
                         <div className={classes.left}>
-                            <Card className={classes.photo} />
+                            <Card className={classes.photo} src={this.state.stu_img}/>
                             <Typography className={classes.leftText}>
-                                學號：405401360
+                                學號：{this.state.stu_id}
                             </Typography>
                             <Button className={classes.button}>
                                 <CameraIcon />
@@ -98,34 +151,34 @@ class Student extends React.Component {
                         <div className={classes.right}>
                             <div>
                                 <Typography className={classes.rightText}>
-                                    姓名：林奕蓓
+                                    姓名：{this.state.stu_name}
                                 </Typography>
                                 <Typography className={classes.rightText}>
-                                    年級：高三
+                                    年級：{this.state.stu_grade}
                                 </Typography>
                                 <Typography className={classes.rightText}>
                                     班別：英文Ａ班、數學Ｃ班
                                 </Typography>
                                 <Typography className={classes.rightText}>
-                                    生日：1998/07/13
+                                    生日：{this.state.stu_birth}
                                 </Typography>
                                 <Typography className={classes.rightText}>
-                                    手機：0988629621
+                                    手機：{this.state.stu_phone}
                                 </Typography>
                                 <Typography className={classes.rightText}>
-                                    學校：私立大同高中
+                                    學校：{this.state.stu_school}
                                 </Typography>
                                 <Typography className={classes.rightText}>
-                                    Email：fanylin0713@gmail.com
+                                    Email：{this.state.stu_email}
                                 </Typography>
                                 <Typography className={classes.rightText}>
-                                    住址：台北市北投區中和街
+                                    住址：{this.state.stu_address}
                                 </Typography>
                                 <Typography className={classes.rightText}>
-                                    聯絡人：林珍瑤
+                                    聯絡人：{this.state.stu_parent}
                                 </Typography>
                                 <Typography className={classes.rightText}>
-                                    聯絡人電話：0937924892
+                                    聯絡人電話：{this.state.stu_parent_phone}
                                 </Typography>
                             </div>
 
